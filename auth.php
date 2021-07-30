570b4454b62105b9b3aa9b992af5e8a78b285e8a
NurbekIsagaliev <71828298+NurbekIsagaliev@users.noreply.github.com>
2021-07-30 18:17:41 +0600

    Add files via upload

---
+++ auth.php
@@ -0,0 +1,15 @@
+<?php
+if ($_COOKIE['user'] == 'Да')
+	setcookie('user','Да',time() - 3600,'/');
+	else
+
+setcookie('user','Да',time() + 3600,'/');
+
+header('Location: /htdocs');
+
+
+
+
+
+
+?>
---
+++ footer.php
@@ -0,0 +1,35 @@
+<footer class="container pt-4 my-md-5 pt-md-5 border-top">
+	 <div class="row">
+	 	<div class="col-12 col-md">
+	 		<img class="mb-2" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="24">
+            <small class="d-block mb-3 text-muted"> &copy;2021</small>
+        </div>
+        <div class="col-6 col-md">
+<h5>Features</h5>
+	 		<ul class="list-unstyled text-small">
+	 			<li> <a class="text-muted" href="#"> Cool stuff </a> </li>
+	 			<li> <a class="text-muted" href="#"> Random feature</a> </li>
+	 			<li> <a class="text-muted" href="#"> Team feature</a> </li>
+	 			<li> <a class="text-muted" href="#"> Stuff of Developers</a> </li>
+	 			</ul>
+	 	</div>
+<div class="col-6 col-md">
+<h5>Resources</h5>
+	 		<ul class="list-unstyled text-small">
+	 			<li> <a class="text-muted" href="#">Resource</a> </li>
+	 			<li> <a class="text-muted" href="#">Resourse name</a> </li>
+	 			<li> <a class="text-muted" href="#">Another resourse</a> </li>
+	 			<li> <a class="text-muted" href="#">Final resourse</a> </li>
+	 		</ul>
+	 	</div>
+<div class="col-6 col-md">
+<h5>About</h5>
+	 		<ul class="list-unstyled text-small">
+	 			<li> <a class="text-muted" href="#">Team</a> </li>
+	 			<li> <a class="text-muted" href="#">Locations</a> </li>
+	 			<li> <a class="text-muted" href="#">Privacy</a> </li>
+	 			<li> <a class="text-muted" href="#">Terms</a> </li>
+</ul>
+</div>	 			
+</div>
+</footer>
